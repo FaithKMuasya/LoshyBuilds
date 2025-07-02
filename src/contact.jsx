8b3c05192e0afb3fc2626164bf0b1fd62ef366a6
FaithKMuasya <loshyfaith@gmail.com>
2025-07-02 15:38:42 +0300

    corrected contact.jsx css importation

--- src/contact.jsx
+++ src/contact.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { MdEmail, MdLocationOn, MdAccessTime } from "react-icons/md";
 import { FaPaperPlane } from "react-icons/fa";
-import "./Contact.css";
+import "./contact.css";
 
 const Contact = () => {
   const form = useRef();
